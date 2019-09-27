ab9f255b23d917b8f30b861c124789a65efbbcb2
inejc <nejc.ilenic@gmail.com>
2019-09-27 23:29:08 +0200

    deploy classifier

--- src/main/scala/io/picnicml/doddlemodel/spamham/DeploySpamClassifier.scala
+++ src/main/scala/io/picnicml/doddlemodel/spamham/DeploySpamClassifier.scala
@@ -1,5 +1,21 @@
 package io.picnicml.doddlemodel.spamham
 
+import io.picnicml.doddlemodel.loadEstimator
+import io.picnicml.doddlemodel.pipeline.Pipeline
+import io.picnicml.doddlemodel.syntax.PredictorSyntax._
+
+import scala.io.StdIn.readLine
+
 object DeploySpamClassifier extends App {
-  println("todo")
+  val sentenceEmbedder = new UniversalSentenceEncoder()
+  val spamClassifier = loadEstimator[Pipeline]("logreg.model")
+  while(true) {
+    val sms = readLine("Input your SMS here: ")
+    val yPred = spamClassifier.predict(sentenceEmbedder.embed(Array(sms)))
+    if (yPred(0) == 1.0)
+      println(s"SMS '$sms' marked as spam")
+    else
+      println(s"SMS '$sms' marked as a legitimate message")
+  }
+  sentenceEmbedder.close()
 }
